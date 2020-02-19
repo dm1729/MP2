a493f6342f649aa915dcbc8d6eb21aef26485de3
Dan Moss <danmoss1729@hotmail.co.uk>
2020-02-19 09:14:23 +0000

    IsingIPS diagnostic checks

--- IsingIPS.R
+++ IsingIPS.R
@@ -19,14 +19,22 @@ Z<-Y #REMOVE FROM CODE AND REPLACE RETURN WITH Y
 Xi <- diag(n) #Initialize sample second moment
 E <- 0*matrix(rep(1,n^2),nrow=n,ncol=n) #Initialize E hat
 Delta <- 0*matrix(rep(1,n^2),nrow=n,ncol=n)
-J <- 0*matrix(rep(1,n^2),nrow=n,ncol=n)
-while (max(abs(mu-x))>=eps | sum(Xi>=M)<2^n | max(abs(Xi-M))>= eps) #Until all three of these do not hold.
+J <- 0*diag(n)
+I <- c(1:(n-1))
+M[lower.tri(M)] <- 0 #We only update one half of Xi so we do this. M symmetric so no issue
+Counter=0 #ADDED COUNTER TO EXIT LOOP FOR DIAGNOSTIC CHECKS
+IfCount=0
+ElseCount=0
+while ((max(abs(mu-x))>=eps | sum(Xi>=M-eps)<n^2 | max(abs(Xi-M))>= eps) & Counter<100) #Until all three of these do not hold.
 {
-  
+  Counter=Counter+1
+  Diagnostic1 <- max(abs(mu-x)) #Diagnostics - Modifying outputs to see
+  Diagnostic2 <- sum(Xi>=M-eps)
+  Diagnostic3 <- max(abs(Xi-M))
   for (i in I) #Initialize E+
   {
-    J=c((i+1):n) #look over i<j
-    for (j in I)
+    II=c((i+1):n) #look over i<j
+    for (j in II)
     {
       if (M[i,j]>x[i]*x[j]) #Has the effect of only looking through E plus (equivalent to the for i,j, in E+ step)
       {
@@ -34,12 +42,14 @@ while (max(abs(mu-x))>=eps | sum(Xi>=M)<2^n | max(abs(Xi-M))>= eps) #Until all t
         J[i,j] <- CanonicalJ(i,j,Y) #See CanonicalJ.R
         if (Delta[i,j]+J[i,j] > 0){
           #Update p by (14)
+          IfCount=IfCount+1
           for (m in N)
           {
             Y[m] <- Y[m]*0.25*(1+C[m,i]*x[i]+C[m,j]*x[j]+M[i,j])/(sum(Y[C[,i]==C[m,i] & C[,j]==C[m,j]])) # denominator marginal
           }
           E[i,j] <- 1 #Puts an edge between i and j in the adjacency matrix
-        }else{
+        } else {
+          ElseCount=ElseCount+1
           #Solve Delta_{ij}(lambda^*) equation
           LambdaStar <- LambdaStar(i,j,x,Y,M,J[i,j]) #J[i,j] not required if I've done this right!
           #Update p by (17)
@@ -57,5 +67,5 @@ while (max(abs(mu-x))>=eps | sum(Xi>=M)<2^n | max(abs(Xi-M))>= eps) #Until all t
   mu <- FirstMoment(Y)
   Xi <- SecondMoment(Y)
 }#End while
-return(Y) #return(Y,E,mu,Xi) Can't return multiple arguments, look over this.
+return(IfCount) #return(Y,E,mu,Xi) Can't return multiple arguments, look over this.
 }
