68f552957c4ef1d73a149f9868e68c4c2c8c657f
Daniel Moss <danmoss1729@hotmail.co.uk>
2020-04-12 11:25:28 +0100

    fixed bug in rank flattening

--- FlatteningRank.R
+++ FlatteningRank.R
@@ -7,8 +7,8 @@ dim(P) <- rep(2,n) #P[2,1,2,1] = Pr(X_4=1,x_3=-1...)=p_{-1,1,-1,1} (inverted)
 #FLATTENINGS
 R <- rep(0,2^{n-1}-n-1)  #(2^n-2n-2)/2
 i <- 0
-for ( j in c( 2,(n-2) ) ){#Need to check subsets of this size
-  for ( k in (choose((n-1),(j-1))) ){ #Choose the ones that aren't '1'
+for ( j in c( 2:(n-2) ) ){#Need to check subsets of this size
+  for ( k in ( c( 1:choose((n-1),(j-1)) ) ) ){ #Choose the ones that aren't '1'
   p <- SubsetToPerm(combn(c(1:n),j)[,k],n) #First k columns gives the subsets we want
   A <- aperm(P,p) #Always fix one index.
   dim(A) <- c(2^j,2^(n-j)) #Flattens
